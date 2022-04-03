513bc522469eeab4e165565d3d9d4813ca523efe
tulakann <tulakan.r@gmail.com>
2022-04-03 13:02:46 +0700

    feat: add styled-components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,19 @@
 import { Code, Heading, Text, VStack } from "@chakra-ui/react";
 import type { NextPage } from "next";
+import styled from "styled-components";
+
+const StyledBox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 320px;
+
+  border: 3px dashed palevioletred;
+  border-radius: 10px;
+
+  padding: 1.25em;
+`;
 
 const Home: NextPage = () => {
   return (
@@ -8,6 +22,11 @@ const Home: NextPage = () => {
       <Text as="p">
         Check <Code>README.md</Code>!
       </Text>
+      <StyledBox>
+        <Text as="p" textAlign="center">
+          This box is created with styled-components
+        </Text>
+      </StyledBox>
     </VStack>
   );
 };
