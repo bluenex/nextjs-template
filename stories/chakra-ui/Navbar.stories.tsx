543720415b26c79ad429c79638ff6fc6b60ee99d
tulakann <tulakan.r@gmail.com>
2022-04-03 20:56:04 +0700

    feat: port main components from ecg-app

--- stories/chakra-ui/Navbar.stories.tsx
+++ stories/chakra-ui/Navbar.stories.tsx
@@ -5,8 +5,25 @@ import Navbar from "../../components/chakra-ui/navbar";
 export default {
   title: "chakra-ui/Navbar",
   component: Navbar,
+  argTypes: {
+    isLoggedIn: { control: "boolean" },
+    isLoadingUserStatus: { control: "boolean" },
+    onClickLogin: { action: "clicked" },
+    onClickLogout: { action: "clicked" },
+  },
 } as ComponentMeta<typeof Navbar>;
 
 const Template: ComponentStory<typeof Navbar> = (args) => <Navbar {...args} />;
 
-export const Default = Template.bind({});
+export const NotLoggedIn = Template.bind({});
+
+export const LoggingIn = Template.bind({});
+LoggingIn.args = {
+  isLoggedIn: false,
+  isLoadingUserStatus: true,
+};
+
+export const LoggedIn = Template.bind({});
+LoggedIn.args = {
+  isLoggedIn: true,
+};
