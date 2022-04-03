ad80146eed02e86d66e106eb545b428318813152
tulakann <tulakan.r@gmail.com>
2022-04-03 12:55:35 +0700

    feat: add chakra-ui and favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,14 @@
+import { Code, Heading, Text, VStack } from "@chakra-ui/react";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <h1>Bluenex Storybook</h1>
-      <p>
-        Check <code>README.md</code>!
-      </p>
-    </div>
+    <VStack direction="column" gap={2} py={8}>
+      <Heading as="h1"> Bluenex Storybook</Heading>
+      <Text as="p">
+        Check <Code>README.md</Code>!
+      </Text>
+    </VStack>
   );
 };
 
