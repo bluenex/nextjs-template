771b9275735190d21a69934dd51495dc4d20ab62
tulakann <tulakan.r@gmail.com>
2022-04-24 11:14:12 +0700

    build: add tailwind type and change config file to ts

---
+++ cra-ts-storybook/tailwind.config.ts
@@ -0,0 +1,11 @@
+import type { TailwindConfig } from "tailwindcss/tailwind-config";
+
+const config: TailwindConfig = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+export default config;
