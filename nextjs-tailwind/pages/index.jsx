c2cfc58f02af01f2b3ab0cef8b7b3d20ee12ca2e
tulakann <tulakan.r@gmail.com>
2022-04-26 15:41:43 +0700

    feat: add tailwind template

---
+++ nextjs-tailwind/pages/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="flex flex-col py-8 justify-center items-center">
+      <h1 className="text-2xl font-bold mb-2">Next.js + Tailwind CSS</h1>
+      <p>Enjoy starting a new project!</p>
+    </div>
+  );
+};
+
+export default Home;
